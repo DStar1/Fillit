fc9fc7e24b869067055e2c1d17fc8cee8efc53ce
Harrison Smith <hasmith@e1z2r9p4.42.us.org>
2017-09-29 14:45:34 -0700

    Planning

---
+++ fillit.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hasmith <hasmith@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/28 16:52:40 by hasmith           #+#    #+#             */
+/*   Updated: 2017/09/29 14:45:02 by hasmith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Find out how many tetrominos they give us and store it in tet_num
+** Find tetromino coordinates and stor them in structs nodes in a linked list named tetrominos
+** Start recursion backtracking by itterating to the last x coordinate and then once the end is reached,
+** itterate the y coordinate the itterate through all the tetrominos, backtrack to make them fit until there are none left.
+** seve this result in a struct and repeat and return the first one that has the least dimensions
+**
+** functions: Solver(iterator, backtrack, ), Validator, find_shape(convert), tet_counter, store_results?, print_result
+*/
+
+void	ft_backtrack(int tet_num, t_list **tettronimos, )
+{
+	/*
+	** start dimensions are 4x4
+	*/
+}
+

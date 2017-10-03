5f3150deff1382d0a3dfc4d184c8803d7386d3fb
kmckee <kmckee@student.42.fr>
2017-10-03 15:58:25 -0700

    tot_char addition

--- validator.c
+++ validator.c
@@ -6,13 +6,13 @@
 /*   By: hasmith <hasmith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/02 20:25:28 by hasmith           #+#    #+#             */
-/*   Updated: 2017/10/02 21:42:51 by hasmith          ###   ########.fr       */
+/*   Updated: 2017/10/03 15:57:46 by kmckee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char	*ft_get_stdin(char *buf, int *i) //pass the adress of i to other functions so it can be manipulated and used
+char	*ft_get_stdin(char *buf, int *tot_char) //pass the adress of i to other functions so it can be manipulated and used
 {
 	int		ret;
 	int		total_size;
@@ -32,15 +32,15 @@ char	*ft_get_stdin(char *buf, int *i) //pass the adress of i to other functions 
 		final = (char *)malloc(sizeof(char) * total_size);
 		final = ft_strcat(tmp, buf);
 	}
-	*i = total_size;
+	*tot_char = total_size;
 	return (final);
 }
 
-int		check_if_valid(char *final, int *i)
+int		check_if_valid(char *final, int *tot_char)
 {
 	// i = nbr of chars in test.txt
 	int j;
-	j = *i;
+	j = *tot_char;
 	int checked = 0;
 	while (j >= 0)
 	{

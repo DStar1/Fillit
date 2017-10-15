fa13de96b571730e18d29db05c602e512c8c3798
Harrison Smith <hasmith@e1z2r9p28.42.us.org>
2017-10-14 18:44:36 -0700

    normed lots

--- fillit.h
+++ fillit.h
@@ -19,16 +19,15 @@
 # include <fcntl.h>
 # include "libft/libft.h"
 
-char	*ft_get_stdin(char *buf, int *i);
-int		check_if_valid(char *final, int *tot_char);
-char	**turn_into_2d(char *final, int *tot_char);
-int		**get_shape(char **two_d_arr, int tet_nubr_npie);
-int     check_if_possible(int **two_d_int_arr, char **two_d_arr, int x, int y);
-//char    **backtrack(char **two_d_arr, int box_size, int ***tree_d_int_arr, int tet_cnt, int tet_nbr_npie);
-char	**solver(int ***tree_d_int_arr, int tet_nbr_npie);
-//char    **create_new_box(int size);
-int		***get_three_d_arr(char **two_d_arr, int tet_nubr_npie);
-
-void	make_grid(int ***three_d_arr, int **two_d, int index);
+char		*ft_get_stdin(char *buf, int *i);
+int			check_if_valid(char *final, int *tot_char);
+char		**turn_into_2d(char *final, int *tot_char);
+int			**get_shape(char **two_d_arr, int tet_nubr_npie);
+int			check_if_possible(int **two_d_int_arr, char **two_d_arr, int x, int y);
+//char    	**backtrack(char **two_d_arr, int box_size, int ***tree_d_int_arr, int tet_cnt, int tet_nbr_npie);
+char		**solver(int ***tree_d_int_arr, int tet_nbr_npie);
+//char   	**create_new_box(int size);
+int			***get_three_d_arr(char **two_d_arr, int tet_nubr_npie);
+void		make_grid(int ***three_d_arr, int **two_d, int index);
 
 #endif
